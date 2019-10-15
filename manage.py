d9ac89328f94c15470e0ef49dd3f923764d07c91
Shaurya <54854259+shaurya133@users.noreply.github.com>
2019-10-15 02:21:22 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -7,7 +7,8 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
-        from django.core.management import execute_from_command_line
+        #from django.core.management import execute_from_command_line
+        import django  # noqa: F401 - The import is needed for diagnostics
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
