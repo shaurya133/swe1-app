71b8eae6dfe1122fa0dc31e20de9e0709a14a0aa
Shaurya <54854259+shaurya133@users.noreply.github.com>
2019-10-15 03:30:38 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
             import django  # noqa: F401 - The import is needed for diagnostics
         except ImportError:
             raise ImportError(
-                              "Couldn't import Django. Are you sure it's installed and "
-                              "available on your PYTHONPATH environment variable? Did you "
-                              "forget to activate a virtual environment?"
+               "Couldn't import Django. Are you sure it's installed and "
+               "available on your PYTHONPATH environment variable? Did you "
+               "forget to activate a virtual environment?"
             )
         raise
     execute_from_command_line(sys.argv)
